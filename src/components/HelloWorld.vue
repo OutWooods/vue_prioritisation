36c40d235c495fc735cf8c62678262896d4effb7
Tom Grand <tom@hegartymaths.com>
2018-09-19 19:21:41 +0100

    add tailwind

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <p class="bg-blue text-white">Testing testing testing</p>
     <p>
       For guide and recipes on how to configure / customize this project,<br>
       check out the
