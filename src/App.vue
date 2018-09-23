056d81af9b903f5d784982f26326cbc693913ff4
Tom Grand <tom@hegartymaths.com>
2018-09-23 17:10:14 +0100

    get all jobs

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,38 @@
   <div id="app">
     <h1>hello</h1>
     <matrix></matrix>
+    <button @click="getJobs">click me</button>
   </div>
 </template>
 
 <script>
-  import Matrix from './components/Matrix.vue'
+  import axios from 'axios'
   import './assets/css/main.css'
+  import Matrix from './components/Matrix.vue'
 
   export default {
     name: 'app',
     components: {
       Matrix,
     },
+    data() {
+      return {
+        jobs: [],
+      }
+    },
+    created() {
+    },
+    methods: {
+      getJobs() {
+        axios.get('http://backend_prioritisation.test/api/jobs')
+          .then((response) => {
+            this.jobs = response.data.jobs;
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+    }
   }
 </script>
 
